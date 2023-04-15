48e3e4f434ea45f8fcb7e8b517ce960c40cb67aa
SpectreOneTwo <125298068+SpectreOneTwo@users.noreply.github.com>
2023-04-14 23:18:52 -0700

    Plane Logic

--- Hero copy/Assets/PlaneBehavior.cs
+++ Hero copy/Assets/PlaneBehavior.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class PlaneBehavior : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public bool moveAroundBoundary = false;
+
+    void Update()
+    {
+        HandleInput();
+        Move();
+    }
+
     private void OnTriggerEnter2D(Collider2D hitinfo)
     {
         //if collides with a hero
@@ -52,5 +61,43 @@ public class PlaneBehavior : MonoBehaviour
             //create a new plane to take its place
             GlobalBehavior.sTheGlobalBehavior.CreatePlane();
         }
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            moveAroundBoundary = !moveAroundBoundary;
+        }
+    }
+
+    void Move()
+    {
+        if (moveAroundBoundary == true)
+        {
+            // move the enemy up until it reaches the world bounds
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+            // check if the enemy is outside of the world bounds
+            if (!IsInsideWorldBounds(transform.position))
+            {
+                // find a new direction within the world bounds
+                Vector3 newDirection = GetRandomDirectionWithinWorldBounds();
+                transform.up = newDirection;
+            }
+        }
+    }
+
+    bool IsInsideWorldBounds(Vector3 position)
+    {
+        return position.x >= -90f && position.x <= 90f && position.y >= -90f && position.y <= 90f;
+    }
+
+    Vector3 GetRandomDirectionWithinWorldBounds()
+    {
+        float x = Random.Range(-1f, 1f);
+        float y = Random.Range(-1f, 1f);
+        Vector3 direction = new Vector3(x, y, 0).normalized;
+        return direction;
     }
 }
